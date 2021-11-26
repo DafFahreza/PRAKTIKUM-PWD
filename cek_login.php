6f2e16be746cb7978c8c19ea52c6646bf7736c25
Daffa Fahreza <62737427+DafFahreza@users.noreply.github.com>
2021-11-26 08:12:13 +0700

    application and security

--- cek_login.php
+++ cek_login.php
@@ -1,25 +1,34 @@
-<?php  
-	include "koneksi.php";
+<?php 
+	session_start();
+	include "../koneksi.php";
 	$id_user = $_POST['id_user'];
-	$pass = md5($_POST['passwd']);
-	$sql = "SELECT * FROM users WHERE id_user = '$id_user' AND password = '$pass'";
+	$pass = md5($_POST['paswd']);
+	$sql = "SELECT * FROM users WHERE id_user='$id_user' AND password='$pass'";
 
-	$login = mysqli_query($con, $sql);
-	$ketemu = mysqli_num_rows($login);
-	$r = mysqli_fetch_array($login);
-	if ($ketemu > 0) {
-		session_start();
+	if ($_POST["captcha_code"]==$_SESSION["captcha_code"]) {
+		// code...
 
-		$_SESSION['iduser'] = $r['id_user'];
-		$_SESSION['passuser'] = $r['password'];
+	$login=mysqli_query($con,$sql);
 
-		echo "USER BERHASIL LOGIN<br>";
-		echo "id user = ", $_SESSION['iduser'], "<br>";
-		echo "password = ", $_SESSION['passuser'], "<br>";
-		echo "<a href = logout.php><b>LOGOUT</b></a></center>";
-	} else {
-		echo "<center>Login Gagal! Username & Password Tidak Benar<br>";
-		echo "<a href = form_login.php><b>ULANGILAGI</b></a></center>";
+	$ketemu=mysqli_num_rows($login);
+	$r=mysqli_fetch_array($login);
+	if ($ketemu >0) {
+		// code...
+		$_SESSION['iduser']=$r['id_user'];
+		$_SESSION['passuser']=$r['password'];
+	echo "USER BERHASIL LOGIN<br>";
+	echo "id user=",$_SESSION['iduser'],"<br>";
+	echo "password=",$_SESSION['passuser'],"<br>";
 	}
-	mysqli_close($con);
-?>
+	else{
+		echo"<center>Login gagl! username &password tidak benar<br>";
+		echo "<a href=form_login.php><b>ULANG LAGI</br></a></center>";
+	}
+	mysql_close($con);
+	}
+	else{
+		echo "<center>Login gagal! Captcha tidak sesuia<br>";
+		echo "<a href=form_login.php><b>ULANG LAGI</br></a></center>";
+	}
+
+ ?>
--- form_login.php
+++ form_login.php
@@ -1,10 +1,14 @@
-<?php  
+<?php 
 	echo "<h2>Login</h2>
-	<form method=POST action=cek_login.php>
+	<form method=post action=cek_login.php>
 	<table>
-	<tr><td>Username</td><td> : <input name='id_user' type='text'></td></tr>
-	<tr><td>Password</td><td> : <input name='passwd' type='text'></td></tr>
-	<tr><td colspan=2><input type='submit' value='LOGIN'></td></tr>
+	<tr><td>Username</td><td>:<input name='id_user'type='text'></td></tr>
+	<tr><td>Password</td><td>:<input name='paswd'type='text'></td></tr>
+	<tr><td>Captcha<br>
+	<img src='captcha.php'/></td><td>:<input name='captcha_code'type='text'></td></tr>
+	<tr><td colspan=2<input type='submit'value='LOGIN'></td></tr>
 	</table>
 	</form>";
-?>
+
+
+ ?>
